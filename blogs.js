5102c041d89e76422d08ba852d30d4bb4c1b9196
Tanaysingh9799 <64830765+Tanaysingh9799@users.noreply.github.com>
2023-01-07 12:44:06 +0530

    Working CRUD operations with get search operations

--- blogs.js
+++ blogs.js
@@ -3,11 +3,14 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+
 var logger = require('morgan');
 var moment = require('moment');
-const mysql = require('mysql2')
+const mysql = require('mysql2');
 var port = 8000
-
+var app = express();
+let JasonParser = app.use(bodyParser.json());
+app.use(express.json())
 var app = express();
 /// Connected to the database//
 const connect = mysql.createConnection({
@@ -15,12 +18,12 @@ const connect = mysql.createConnection({
     port:3306,
     user:'root',
     password:'123456654321',
-    database:'sys' 
+    database:'sys'
 })
 ////////////////////////////////////////////////////////////////
 connect.connect((error)=>{
     if(error){
-      console.log("unable to connect")
+      console.log("Unable to connect...")
     }
     else{
       console.log('Connected to the database succesfully!')
@@ -29,19 +32,20 @@ connect.connect((error)=>{
   ////////////////////////////////////////////////////////////////
   ///FIRST PAGE //
 app.get("/",(req,res)=>{
-    res.send("Blog Page without DB")
+    res.send("[Blog Page without DB]")
 })
 
 ////////////////////////////////////////////////////////////////
 /////GETTING DATA FROM DB /////
- app.get("/admin",(req,res)=>{
+ app.get("/admin",(req,res,result)=>{
     connect.query('SELECT * FROM blog',(err,rows)=>{
         if(err){
             console.log(err)
         }
         else{
             res.send({
-                ALL_Blogs: rows
+                ALL_Blogs: rows,
+                Status:result
             })
             
         }
@@ -51,13 +55,14 @@ app.get("/",(req,res)=>{
 /////////////////////
 //DELETING DATA FROM DB //
 app.delete("/admin/:id",(req,res)=>{
-    connect.query('DELETE FROM blog WHERE id =? ',[req.params.id],(err,rows)=>{
+    connect.query('DELETE FROM blog WHERE id =? ',[req.params.id],(err,rows,result)=>{
         if(err){
             console.log(err)
         }
         else{
             res.send({
-                ALL_Deleted_Blogs: rows
+                Message:"Blog Deleted",
+                Status:result
             })
             
         }
@@ -66,34 +71,32 @@ app.delete("/admin/:id",(req,res)=>{
  })
 
 
-
-
-
-
-
-
 ////////////////////
 //ADDING DATA TO DB //
-app.post("/admin",(req,res)=>{
-
-    var update = req.body
-    var updatedData = [update.title,update.description,update.author,update.views,update.uploadedOn]
-    connect.query('INSERT INTO blog(title, description , author , views, uploadedOn) VALUES = (?)',[updatedData],(err,rows)=>{
+app.post("/admin",bodyParser.json(),(req,res,)=>{
+   const title = req.body.title
+   const description = req.body.description
+   const author = req.body.author
+   const views = req.body.views
+   const uploadedOn = moment().format('YYYY/MM/DD')
+    connect.query(`INSERT INTO blog(title,description,author,views,uploadedOn) VALUES(?,?,?,?,?)`,[title,description,author,views,uploadedOn],(err,result)=>{
       if(err){
-            console.error("Failed to add blog")
+            console.error(err)
      }
        else{
-           console.log(rows)
-           res.json(rows)
+           res.json({
+            Message:"Blog Added",
+            Status:result
+           })
    }
    })
 })
 
 
-///FIND DATA BY ID//
-app.get('/admin/find/:id', (req,res)=>{
+//FIND DATA BY ID//
+app.get('/admin/findbyId/:id', (req,res)=>{
     let id = req.params.id;
-    const query = 'SELECT * FROM blog WHERE id =?';
+    const query = 'SELECT * FROM blog WHERE id =(?)';
     connect.query(query,[id],(err,rows)=>{
         if(err){
             console.error(err)
@@ -101,23 +104,43 @@ app.get('/admin/find/:id', (req,res)=>{
         }
         else{
             console.log(rows)
-            res.json(rows)
+            res.json({Blog:rows})
         }
     });
 })
 
-////////////////////////////////////////////////////////////////
-//UPDATING DATA TO DB //
-app.put('/admin/change/:id',(req,res)=>{ 
 
-    
-    var update = req.body
-    var updatedData = [update.title,update.description,update.author,update.views,update.uploadedOn]
-    const query = 'UPDATE INTO blog VALUES = ?';
-    let values = [title, description, author, views, uploadedOn];
-    connect.query(query,[updatedData],(err,rows)=>{
+//FIND DATA BY VIEWS//
+app.get('/admin/findbyViews/:views', (req,res)=>{
+    let views = req.params.views;
+    const query = 'SELECT * FROM blog WHERE views = (?)';
+    connect.query(query,[views],(err,rows)=>{
+        if(err){
+            console.error(err)
+            
+        }
+        else{
+            res.json({
+                Message:"Blogs With "+views+" views: ",
+                    Blog:rows})
+        }
+    });
+})
+
+/////////////////////////////////////////////////////
+//UPDATING DATA TO DB //
+ app.put('/admin/change/id/:id',bodyParser.json(),(req,res)=>{ 
+    const id = req.params.id
+    const title = req.body.title
+    const description = req.body.description
+    const author = req.body.author
+    const views = req.body.views
+    const uploadedOn = moment().format('YYYY/MM/DD')
+    console.log([title, description, author, views, uploadedOn])
+    const query = 'UPDATE  blog SET title=? , description=?, author=?, views=?, uploadedOn=? WHERE id = (?)';
+      connect.query(query,[title,description,author,views,uploadedOn,id],(err,rows)=>{
       if(err){
-            console.error("Fail to add blog")
+            console.error(err)
      }
        else{
            console.log(rows)
